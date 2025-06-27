7f8c4937adc833799656daf0a2a6232f6974c956
CreatFun <Bukin.Ivan@urfu.me>
2025-06-28 02:17:14 +0500

    Заглушка на экране профиля

--- app/src/main/java/com/example/androidpractice/profile/presentation/screens/ProfileScreen.kt
+++ app/src/main/java/com/example/androidpractice/profile/presentation/screens/ProfileScreen.kt
@@ -1,7 +1,12 @@
 package com.example.androidpractice.profile.presentation.screens
 
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.github.terrakok.modo.Screen
 import com.github.terrakok.modo.ScreenKey
 import com.github.terrakok.modo.generateScreenKey
@@ -14,6 +19,13 @@ class ProfileScreen(
 
     @Composable
     override fun Content(modifier: Modifier) {
+
+        Text(
+            "Экран профиля. Реализуется в следующих практиках",
+            modifier
+                .padding(24.dp)
+                .fillMaxSize()
+        )
 
 
     }
