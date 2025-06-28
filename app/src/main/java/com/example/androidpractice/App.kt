9d4fbf5a4f5d0c233bd3cd9ed2217b37b17b1391
CreatFun <Bukin.Ivan@urfu.me>
2025-06-28 19:11:27 +0500

    модуль работы с сетью подключен

--- app/src/main/java/com/example/androidpractice/App.kt
+++ app/src/main/java/com/example/androidpractice/App.kt
@@ -1,6 +1,7 @@
 package com.example.androidpractice
 
 import android.app.Application
+import com.example.androidpractice.di.networkModule
 import com.example.androidpractice.di.rootModule
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
@@ -13,7 +14,7 @@ class App: Application() {
         startKoin {
             androidLogger()
             androidContext(this@App)
-            modules(rootModule)
+            modules(rootModule, networkModule)
         }
     }
 }
