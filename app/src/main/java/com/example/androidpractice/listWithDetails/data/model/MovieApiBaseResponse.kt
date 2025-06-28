ff84b5010f80184e4b53728df3daf60c68dfd8d4
CreatFun <Bukin.Ivan@urfu.me>
2025-06-28 19:12:59 +0500

    Добавлены модели для сериализации ответов API (нужны изменения для работы с выбранной API)

--- app/src/main/java/com/example/androidpractice/listWithDetails/data/model/MovieApiBaseResponse.kt
+++ app/src/main/java/com/example/androidpractice/listWithDetails/data/model/MovieApiBaseResponse.kt
@@ -2,7 +2,7 @@ package com.example.androidpractice.listWithDetails.data.model
 
 import com.google.gson.annotations.SerializedName
 
-class MovieApiBaseResponse {
+abstract class MovieApiBaseResponse {
     @SerializedName("Response")
     val response: Boolean = false
 
--- app/src/main/java/com/example/androidpractice/listWithDetails/data/model/MoviesSearchResponse.kt
+++ app/src/main/java/com/example/androidpractice/listWithDetails/data/model/MoviesSearchResponse.kt
@@ -1,2 +1,21 @@
 package com.example.androidpractice.listWithDetails.data.model
 
+import com.google.gson.annotations.SerializedName
+
+class MoviesSearchResponse(
+    @SerializedName("titles")
+    val titles: List<MoviesShortResponse>?
+): MovieApiBaseResponse()
+
+class MoviesShortResponse(
+    @SerializedName("id")
+    val id: String?,
+    @SerializedName("primary_title")
+    val primary_title: String?,
+    @SerializedName("start_year")
+    val start_year: String?,
+    @SerializedName("type")
+    val type: String?,
+    @SerializedName("primary_image")
+    val primary_image: String?
+)
